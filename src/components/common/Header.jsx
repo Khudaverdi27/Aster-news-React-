59ec115ab3d7289e04e1fc200661fb71a65b1439
Xudaverdi <xudaverdi.sh@gmail.com>
2024-01-27 01:47:38 +0400

    test:input focus problem solving

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
         <ToggleMenu />
         <FormSearch />
       </div>
-      <div className="flex items-center ml-1 md:ml-16 lg:space-x-5  lg:mt-0 mt-5 lg:justify-between lg:mx-3 ">
+      <div className="flex items-center sm:justify-center md:justify-normal  ml-1 md:ml-16 lg:space-x-5  lg:mt-0 mt-5 lg:justify-between lg:mx-3 ">
         <div className="order-1 lg:text-[16px] md:ml-2 text-sm lg:ml-10">
           {token ? (
             <DropdownMenu
