771e9b1f9014ada52c28af590873efae802a3a22
Xudaverdi <xudaverdi.sh@gmail.com>
2024-02-02 01:50:18 +0400

    fixed: about page is divided to components

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,6 @@
-import { FaRegFileAlt } from "react-icons/fa";
-import { FiUsers } from "react-icons/fi";
-import { IoPersonSharp } from "react-icons/io5";
-import { LuAlarmClock } from "react-icons/lu";
+import CardInfo from "./CardInfo";
 
-function AboutPage() {
+function AboutPage({ info }) {
   return (
     <section className="  font-poppins darkMode sm:mt-0 ">
       <div className="max-w-6xl py-4 mx-auto lg:py-6 md:px-6 mt-24">
@@ -24,58 +21,9 @@ function AboutPage() {
                 enim ad minim veniam Lorem ipsum dolor sit amet.
               </p>
               <div className="flex flex-wrap items-center">
-                <div className="w-full px-4 mb-6 sm:w-1/2 md:w-1/2 lg:mb-6">
-                  <div className="p-6 bg-white dark:bg-amberBlack">
-                    <span className="text-blue-500 text-4xl ">
-                      <FaRegFileAlt />
-                    </span>
-                    <p className="mt-4 mb-2 text-3xl font-bold text-gray-700 dark:text-white">
-                      2097
-                    </p>
-                    <h2 className="text-sm text-gray-700 dark:text-white">
-                      Projects and Plans
-                    </h2>
-                  </div>
-                </div>
-                <div className="w-full px-4 mb-6 sm:w-1/2 md:w-1/2 lg:mb-6">
-                  <div className="p-6 bg-white dark:bg-amberBlack">
-                    <span className="text-blue-500 text-4xl ">
-                      <IoPersonSharp />
-                    </span>
-                    <p className="mt-4 mb-2 text-3xl font-bold text-gray-700 dark:text-white">
-                      3,590
-                    </p>
-                    <h2 className="text-sm text-gray-700 dark:text-white">
-                      Helped people
-                    </h2>
-                  </div>
-                </div>
-                <div className="w-full px-4 mb-6 sm:w-1/2 md:w-1/2 lg:mb-6">
-                  <div className="p-6 bg-white dark:bg-amberBlack">
-                    <span className="text-blue-500 text-4xl ">
-                      <FiUsers />
-                    </span>
-                    <p className="mt-4 mb-2 text-3xl font-bold text-gray-700 dark:text-white">
-                      74
-                    </p>
-                    <h2 className="text-sm text-gray-700 dark:text-white">
-                      Volunteer
-                    </h2>
-                  </div>
-                </div>
-                <div className="w-full px-4 mb-6 sm:w-1/2 md:w-1/2 lg:mb-6">
-                  <div className="p-6 bg-white dark:bg-amberBlack">
-                    <span className="text-blue-500 text-4xl ">
-                      <LuAlarmClock />
-                    </span>
-                    <p className="mt-4 mb-2 text-3xl font-bold text-gray-700 dark:text-white">
-                      100
-                    </p>
-                    <h2 className="text-sm text-gray-700 dark:text-white">
-                      Timing
-                    </h2>
-                  </div>
-                </div>
+                {info.map((item, index) => (
+                  <CardInfo key={index} items={item} />
+                ))}
               </div>
             </div>
           </div>
